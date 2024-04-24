18e6d9f55b673462a41e73ef46a3c94444eb7805
kssgarcia <kssgarcia@outlook.com>
2024-04-24 13:55:16 -0500

    Renaming files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './App.css'
 
 import vertexShader from './shaders/vertex.glsl';
 import fragmentShader from './shaders/fragment.glsl';
