72bfe533e02552e9c5faa90138c1f892a9cad26d
kssgarcia <kssgarcia@outlook.com>
2024-04-25 12:20:26 -0500

    organizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './App.css'
+import './css/App.css'
 
 import axios from 'axios';
 import xml2js from 'xml2js';
--- src/Project.jsx
+++ src/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import './Project.css';
+import './css/Project.css';
 
 const Project = () => {
     const location = useLocation();
